bf8051c8b11e058664d9e84f3c5d93c82713830d
Harrison Kim <kim.harr@northeastern.edu>
2023-03-18 11:22:36 -0400

    refactored controller to be testable

--- src/controller/CommandController.java
+++ src/controller/CommandController.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import controller.commands.AddImageToLayerCommand;
 import controller.commands.AddLayerCommand;
+import controller.commands.Controller;
 import controller.commands.ProjectCommand;
 import controller.commands.SaveImageCommand;
 import controller.commands.SaveProjectCommand;
@@ -13,44 +14,51 @@ import model.pixels.Pixel;
 import model.projects.CollageProject;
 import model.projects.ProjectModel;
 
-public class CommandController {
-  public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    ProjectModel<Pixel> projectModel = null;
+public class CommandController implements Controller {
+  private final Scanner scanner;
+  private ProjectModel<Pixel> model;
+
+  public CommandController(Readable input) {
+    this.scanner = new Scanner(input);
+  }
+
+  @Override
+  public void executeProgram() {
     ProjectCommand command = null;
-    while(s.hasNext()) {
-      String option = s.next();
+
+    while (this.scanner.hasNext()) {
+      String option = scanner.next();
       try {
         switch (option) {
           case "quit":
             return;
           case "new-project":
-            projectModel = new CollageProject(s.next(), s.nextInt(), s.nextInt());
+            this.model = new CollageProject(scanner.next(), scanner.nextInt(), scanner.nextInt());
             break;
           case "save-project":
             command = new SaveProjectCommand();
             break;
           case "add-layer":
-            command = new AddLayerCommand(s.next());
+            command = new AddLayerCommand(scanner.next());
             break;
           case "set-filter":
-            command = new SetFilterCommand(s.next(), s.next());
+            command = new SetFilterCommand(scanner.next(), scanner.next());
             break;
           case "add-image-to-layer":
-            command = new AddImageToLayerCommand(s.next(), s.next(), s.nextInt(), s.nextInt());
+            command = new AddImageToLayerCommand(scanner.next(), scanner.next(),
+                    scanner.nextInt(), scanner.nextInt());
             break;
           case "load-project":
-
             break;
           case "save-image":
-            command = new SaveImageCommand(s.next());
+            command = new SaveImageCommand(scanner.next());
             break;
           default:
             System.out.println("Unknown command");
             break;
         }
         if (command != null) {
-          command.executeCommand(projectModel);
+          command.executeCommand(this.model);
         }
       } catch (InputMismatchException ignored) {
         System.out.println("Something went wrong");
---
+++ src/controller/commands/Controller.java
@@ -0,0 +1,11 @@
+package controller.commands;
+
+/**
+ * Represents a controller for the collage project.
+ */
+public interface Controller {
+  /**
+   * Executes the collage project.
+   */
+  void executeProgram();
+}
