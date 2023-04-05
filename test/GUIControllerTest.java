0616a5938f77a23c3332d14da039369cc8ee2426
Harrison Kim <kim.harr@northeastern.edu>
2023-04-05 15:49:15 -0400

    finished GUI controller mock tests

--- test/GUIControllerTest.java
+++ test/GUIControllerTest.java
@@ -1,17 +1,21 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 import controller.ControllerFeatures;
 import controller.GUIController;
+import model.filters.RedComponentFilter;
 import model.images.ImageModel;
 import model.pixels.Pixel;
 import model.projects.CollageProject;
+import model.projects.ProjectModel;
 import view.CollageGUIView;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests the controller to ensure that it can handle inputs from the view correctly.
@@ -30,7 +34,7 @@ public class GUIControllerTest {
   /**
    * Mock GUI view to check that controller-view interactions are as expected.
    */
-  private class GUIViewMock implements CollageGUIView<Pixel> {
+  private static class GUIViewMock implements CollageGUIView<Pixel> {
     private final Appendable log;
 
     /**
@@ -44,7 +48,7 @@ public class GUIControllerTest {
     @Override
     public void renderErrorMessage(String message) {
       try {
-        this.log.append("Error: " + message + "\n");
+        this.log.append("Error: ").append(message).append("\n");
       } catch (IOException ignored) {
       }
     }
@@ -87,7 +91,17 @@ public class GUIControllerTest {
     this.controller.applyFilter("blah", "red-component");
     assertEquals("Controller features added to the view\n" +
             "Layer->Filter Mapping:\ndefault-layer normal\n" +
-            "Error: Selected layer does not exist.\n", this.log.toString());
+            "Error: Selected layer or filter does not exist.\n", this.log.toString());
+  }
+
+  @Test
+  public void testApplyFilterInvalidFilter() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.applyFilter("default-layer", "foo");
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n" +
+            "Error: Selected layer or filter does not exist.\n", this.log.toString());
   }
 
   @Test
@@ -98,5 +112,126 @@ public class GUIControllerTest {
     assertEquals("Controller features added to the view\n" +
             "Layer->Filter Mapping:\ndefault-layer normal\n" +
             "Layer->Filter Mapping:\ndefault-layer red-component\n", this.log.toString());
+  }
+
+  @Test
+  public void testMakeNewProjectInvalidDimensions() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.makeNewProject(-1, 100);
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n" +
+            "Error: Please enter non-negative, non-zero integers.\n", this.log.toString());
+    this.controller.makeNewProject(300, -20);
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n" +
+            "Error: Please enter non-negative, non-zero integers.\n" +
+            "Error: Please enter non-negative, non-zero integers.\n", this.log.toString());
+  }
+
+  @Test
+  public void testMakeNewProject() {
+    Appendable log = new StringBuilder();
+    ProjectModel<Pixel> project = new CollageProject(200, 300);
+    project.addLayer("layer-one");
+    project.addLayer("layer-two");
+    project.setLayerFilter("layer-two", new RedComponentFilter());
+    this.controller = new GUIController(project, new GUIViewMock(log));
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\n" +
+            "layer-one normal\n" +
+            "layer-two red-component\n" +
+            "default-layer normal\n", log.toString());
+    this.controller.makeNewProject(100, 100);
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\n" +
+            "layer-one normal\n" +
+            "layer-two red-component\n" +
+            "default-layer normal\n" +
+            "Layer->Filter Mapping:\n" +
+            "default-layer normal\n", log.toString());
+  }
+
+  @Test
+  public void testAddImageToLayerInvalidRowCol() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.addImageToLayer("./test/images/cat.ppm", "default-layer",
+            -3, 5);
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n" +
+            "Error: Please enter non-negative numbers for the displacement.\n", this.log.toString());
+  }
+
+  @Test
+  public void testAddImageToLayerInvalidFile() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.addImageToLayer("./test/images/cat.xyz", "default-layer",
+            0, 0);
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n" +
+            "Error: Unsupported file extension or unsupported file format or " +
+            "image will be out-of-bounds of the screen.\n", this.log.toString());
+  }
+
+  @Test
+  public void testAddImageToLayer() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.addImageToLayer("./test/images/cat.ppm", "default-layer",
+            0, 0);
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+  }
+
+  @Test
+  public void testAddLayerAlreadyExists() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.addLayer("default-layer");
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n" +
+            "Error: Layer default-layer already exists!\n", this.log.toString());
+  }
+
+  @Test
+  public void testAddLayer() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.addLayer("layer-one");
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n" +
+            "Layer->Filter Mapping:\nlayer-one normal\n" +
+            "default-layer normal\n", this.log.toString());
+  }
+
+  @Test
+  public void testSaveProject() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.saveProject("./test/saved-project.collage");
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    File file = new File("./test/saved-project.collage");
+    assertTrue(file.delete());
+  }
+
+  @Test
+  public void testSaveImage() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    this.controller.saveImage("./test/saved-image.ppm");
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());
+    File file = new File("./test/saved-image.ppm");
+    assertTrue(file.delete());
+  }
+
+  @Test
+  public void testLoadProject() {
+    assertEquals("Controller features added to the view\n" +
+            "Layer->Filter Mapping:\ndefault-layer normal\n", this.log.toString());;
   }
 }
